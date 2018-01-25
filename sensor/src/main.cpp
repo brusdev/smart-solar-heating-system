fe75c70630124143d5c6a0db78029363c85a5c98
brusdev <brusdev@gmail.com>
2018-01-25 04:23:56 +0100

    Test DS18B20.

--- sensor/src/main.cpp
+++ sensor/src/main.cpp
@@ -1,8 +1,3 @@
-#define ESP8266
-#define LWIP
-#define LWIP_IPV4=1
-#define LWM2M_WITH_LOGS
-
 /*****
  ESP8266 Led via lwm2m
  Blink the blue LED on the ESP-01 or the seperate red or blue led on the nodeMCU module
@@ -22,6 +17,8 @@
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #include "wakaama_simple_client.h"
 #include "wakaama_network.h"
@@ -30,6 +27,90 @@
 #include "led_object.h"
 
 #define LWM2M_SERVER_ADDR "coap://192.168.10.189"
+
+#define ONE_WIRE_BUS D3  // DS18B20 pin
+#define ONE_WIRE_MAX_DEV 15 //The maximum number of devices
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature DS18B20(&oneWire);
+int numberOfDevices; //Number of temperature devices found
+DeviceAddress devAddr[ONE_WIRE_MAX_DEV];  //An array device temperature sensors
+float tempDev[ONE_WIRE_MAX_DEV]; //Saving the last measurement of temperature
+float tempDevLast[ONE_WIRE_MAX_DEV]; //Previous temperature measurement
+long lastTemp; //The last measurement
+const int durationTemp = 5000; //The frequency of temperature measurement
+
+//------------------------------------------
+//Convert device id to String
+String GetAddressToString(DeviceAddress deviceAddress){
+  String str = "";
+  for (uint8_t i = 0; i < 8; i++){
+    if( deviceAddress[i] < 16 ) str += String(0, HEX);
+    str += String(deviceAddress[i], HEX);
+  }
+  return str;
+}
+
+//Setting the temperature sensor
+void SetupDS18B20(){
+  DS18B20.begin();
+
+  Serial.print("Parasite power is: "); 
+  if( DS18B20.isParasitePowerMode() ){ 
+    Serial.println("ON");
+  }else{
+    Serial.println("OFF");
+  }
+  
+  numberOfDevices = DS18B20.getDeviceCount();
+  Serial.print( "Device count: " );
+  Serial.println( numberOfDevices );
+
+  lastTemp = millis();
+  DS18B20.requestTemperatures();
+
+  // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++){
+    // Search the wire for address
+    if( DS18B20.getAddress(devAddr[i], i) ){
+      //devAddr[i] = tempDeviceAddress;
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: " + GetAddressToString(devAddr[i]));
+      Serial.println();
+    }else{
+      Serial.print("Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address. Check power and cabling");
+    }
+
+    //Get resolution of DS18b20
+    Serial.print("Resolution: ");
+    Serial.print(DS18B20.getResolution( devAddr[i] ));
+    Serial.println();
+
+    //Read temperature from DS18b20
+    float tempC = DS18B20.getTempC( devAddr[i] );
+    Serial.print("Temp C: ");
+    Serial.println(tempC);
+  }
+}
+
+//Loop measuring the temperature
+void TempLoop(long now){
+  if( now - lastTemp > durationTemp ){ //Take a measurement at a fixed time (durationTemp = 5000ms, 5s)
+    for(int i=0; i<numberOfDevices; i++){
+      float tempC = DS18B20.getTempC( devAddr[i] ); //Measuring temperature in Celsius
+        Serial.print("TempLoop C: ");
+        Serial.println(tempC);
+      tempDev[i] = tempC; //Save the measured value to the array
+    }
+    DS18B20.setWaitForConversion(false); //No waiting for measurement
+    DS18B20.requestTemperatures(); //Initiate the temperature measurement
+    lastTemp = millis();  //Remember the last time measurement
+  }
+}
+
 
 lwm2m_context_t * client_context;
 void setup() {
@@ -91,6 +172,10 @@ void setup() {
         printf("Failed to open socket\n");
         
     lwm2m_add_server(123, LWM2M_SERVER_ADDR, 100, false, NULL, NULL, 0);
+
+
+ //Setup DS18b20 temperature sensor
+  SetupDS18B20();
 }
 
 void loop() {
@@ -106,5 +191,8 @@ void loop() {
 
     lwm2m_network_native_sock(client_context, 0);
     lwm2m_network_process(client_context);
-}
+
+    long t = millis();
+    TempLoop( t );
+  }
  
